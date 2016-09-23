08fbd16e334b9729b1cdc913f303e48404a8321d
Emil Bergström <emil.bergstrom1@gmail.com>
2016-09-23 21:00:45 +0200

    removed tempfiles

--- temp/bookcabin.cljs
+++
@@ -1,5 +0,0 @@
-(ns public$.bookcabin
-    (:require [reagent.core :as reagent :refer [atom]]))
-
-
-(defn component [])
--- temp/cabin.cljs
+++
@@ -1,5 +0,0 @@
-(ns public$.cabin
-    (:require-macros [cljs.core.async.macros :refer [go]])
-    (:require  [cljs.core.async :refer [<!]]
-               [reagent.core :as reagent :refer [atom]]
-               [cljs-http.client :as http]))
