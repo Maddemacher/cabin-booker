c69c0d1f0a39eb53ac91ebf330c559ec6f0cfc0c
Emil Bergström <emil.bergstrom1@gmail.com>
2016-10-02 22:43:38 +0200

    log seeder, adding elk stack for logs, trying to get filbeat to mine logs@

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
 # cabin-booker
 Moms cabin booker
 
-NodeJs Cljs cabin booker
+NodeJs Cljs cabinbooker
+
+## How to run
+
+This application levrages docker-compose so deployment is very easy.
+
+After docker and docker-compose is installed run:
+
+- docker-compose build
+- docker-compose up
+
+
+## Ports used
+
+- cabinbooker : 1337
+- lumberjack : 5000
+- kibana : 5601
--- private/services/logger.js
+++ private/services/logger.js
@@ -1,4 +1,12 @@
+const fs = require('fs');
 
-exports.log = msg => {
-  console.log(msg);
+exports.log = (msg, severity = "INFO") => {
+  let message = `Cabin-Booker | ${new Date()} | ${severity} | ${msg}\n\r'`;
+
+  console.log(message);
+
+  fs.appendFile('/var/log/cabinbooker.log', message, error => {
+    if (error)
+      console.log(`Unable to write to logfile error: ${error}`);
+  });
 };
